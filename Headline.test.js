41649387778bc98e45de30e5e65dbd9cfd6b60e3
Montana Mendy <montana@linux.com>
2022-06-10 09:07:28 -0700

    headline test

---
+++ Headline.test.js
@@ -0,0 +1,12 @@
+import { render } from '@redwoodjs/testing'
+
+import Headline from './Headline'
+
+describe('Headline', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<Headline />)
+    }).not.toThrow()
+  })
+})
+@Montana
